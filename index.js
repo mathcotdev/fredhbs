7cd06e3153ed85f2375082d3fbdc5d2d9034a5bd
Alfredmus11 <alfredmushagalusam@gmail.com>
2023-12-10 15:00:36 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const bodyParser = require("body-parser")
+const express = require("express")
+const app = express()
+require("dotenv").config()
+const hbs = require("hbs")
+
+
+app.set("view engine", "hbs")
+app.use(bodyParser.json())
+app.listen(process.env.PORT, ()=>{console.log("http://localhost:"+4000)})
+
+app.get("/", (req, res)=>{res.render("index")})
